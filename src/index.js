93cb360a6997074fd258ce464337cbbd4b25965a
Waleed Saad Mohamed Alkalla <87426985+Abohamssa@users.noreply.github.com>
2022-01-02 22:58:12 +0200

    get the full year

--- src/index.js
+++ src/index.js
@@ -11,12 +11,12 @@ import React from "react";
 import ReactDom from "react-dom";
 
 const name = "Waleed Saad";
-const date = document.write(new Date().getFullYear());
+const year = new Date().getFullYear();
 
 ReactDom.render(
     <div>
-        <p>Created by {name}</p>
-        <p> Copyright {date}</p>
+        <p> Created by {name} </p>
+        <p> Copyright {year} </p>
     </div>,
     document.getElementById("root")
 );
